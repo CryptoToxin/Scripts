4db569c0007d66dc3650f82e2d99cfdae8d77cf8
Chris <davies.cw88@gmail.com>
2019-09-28 13:39:17 +0100

    added posh links to profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
+Set-Location C:\		# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/set-location
+Import-Module posh-git		# https://github.com/dahlbyk/posh-git
+Import-Module oh-my-posh	# https://github.com/JanDeDobbeleer/oh-my-posh
 Set-Theme paradox
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-Location C:\
 
